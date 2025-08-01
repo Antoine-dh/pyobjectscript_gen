022895d6ba6996b6033cc5c7833e7e4b978d4a05
antoine_dh <antoine.dhollande@talan.com>
2025-08-01 19:51:19 +0200

    Added CSV example

---
+++ examples/csv_import.py
@@ -0,0 +1,44 @@
+from pyobjectscript_gen.cls import *
+import csv
+import sys
+
+OUTPUT_DIR = "examples/generated/csv/"
+
+PACKAGE = "Demo.CSV"
+
+# generic type name map to Objectscript specialized types for example
+TYPE_MAP = {
+    "Integer": "%Integer",
+    "String": "%String",
+    "Numeric": "%Numeric",
+    "Boolean": "%Boolean"
+}
+
+if __name__=="__main__":
+    if len(sys.argv[1]) < 2:
+        print("A CSV file as argument is required")
+        exit(1)
+
+    classes: dict[str, Class] = {}
+
+    with open(sys.argv[1], newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            class_name, prop_name, prop_type, maxlen, required = row.values()
+            if class_name not in classes:
+               classes[class_name] = Class(f"{PACKAGE}.{class_name}", extends=["%Persistent"])
+            prop = Property(
+                name=prop_name,
+                type=TYPE_MAP[prop_type]
+            )
+            if maxlen:
+                prop.params["MAXLEN"] = int(maxlen)
+            if required.upper() == "TRUE":
+                prop.keywords["Required"] = None
+            classes[class_name].components.append(prop)
+
+        for name, cls in classes.items():
+            path = f"{OUTPUT_DIR}{name}.cls"
+            print(f"generating file {path} ...")
+            with open(path, 'w') as file:
+                cls.generate(file)
